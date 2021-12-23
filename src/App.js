443bb5853944fefeb72fbdc2d91c1774c38766fa
alainagossett <91034239+alainagossett@users.noreply.github.com>
2021-12-22 19:49:03 -0700

    creates weatherData component and imports to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import weatherData from "./weatherData";
+
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1> 
+      <h1>My WeatherIcons App</h1>
     </div>
   );
 }
