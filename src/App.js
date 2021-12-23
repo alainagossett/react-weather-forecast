2161514e072d837e209f2c86e40fb4439cb70a65
alainagossett <91034239+alainagossett@users.noreply.github.com>
2021-12-22 19:55:07 -0700

    adds WeatherForecast component and imports to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import weatherData from "./weatherData";
+import WeatherForecast from "./components/WeatherForecast";
 
 import "./styles.css";
+
+// console.log(weatherData);
 
 export default function App() {
   return (
     <div className="App">
       <h1>My WeatherIcons App</h1>
+      <WeatherForecast />
     </div>
   );
 }
---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,9 @@
+function WeatherForecast(props) {
+  return (
+    <>
+      <h1>Weather Forecast</h1>
+    </>
+  );
+}
+
+export default WeatherForecast;
