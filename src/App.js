d109ba7700b98cb7008f0961f0bef33a6837c81e
alainagossett <91034239+alainagossett@users.noreply.github.com>
2021-12-27 17:37:53 -0700

    loops over weatherData and renders forecast

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ import "./styles.css";
 
 // console.log(weatherData);
 
+const weather = weatherData.map((info, index) => {
+  return (
+    <div className="weather">
+      <img src={info.img} alt="" />
+      <p>
+        <span>conditions:</span> {info.conditions}
+      </p>
+      <p>
+        <span>time:</span> {info.time}
+      </p>
+    </div>
+  );
+});
+
 export default function App() {
   return (
     <div className="App">
-      <h1>My WeatherIcons App</h1>
       <WeatherForecast />
+      <section className="weather">{weather}</section>
+      {/* <section className="weather">
+        <h1>My WeatherIcons App</h1>
+      </section> */}
     </div>
   );
 }
--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 function WeatherForecast(props) {
   return (
     <>
